e2ab1b1dc27492beee5919290bca4a2d13f2067e
Anton Khorev <tony29@yandex.ru>
2016-08-23 21:12:10 +0300

    babel transform for String.prototype.repeat always work for string literals

--- babel-plugin-transform-string-prototype-repeat.js
+++ babel-plugin-transform-string-prototype-repeat.js
@@ -3,23 +3,24 @@
 module.exports=({ types: t })=>({
 	visitor: {
 		MemberExpression(path,{file}) {
+			const hasStringTypeDeclaration=(comments)=>(comments && comments.some(
+				({value})=>(value==':string'))
+			)
 			if (
-				t.isIdentifier(path.node.property,{ name: 'repeat' })
+				t.isIdentifier(path.node.property,{ name: 'repeat' }) && (
+					t.isStringLiteral(path.node.object) ||
+					hasStringTypeDeclaration(path.node.object.trailingComments)
+				)
 			) {
-				const comments=path.node.object.trailingComments
-				if (comments && comments.some(
-					({value})=>(value==':string'))
-				) {
-					path.replaceWith(
+				path.replaceWith(
+					t.callExpression(
 						t.callExpression(
-							t.callExpression(
-								t.identifier('require'),
-								[t.stringLiteral('crnx-build/babel-helpers/string-prototype-repeat.es5')]
-							),
-							[path.node.object]
-						)
+							t.identifier('require'),
+							[t.stringLiteral('crnx-build/babel-helpers/string-prototype-repeat.es5')]
+						),
+						[path.node.object]
 					)
-				}
+				)
 			}
 		}
 	}
